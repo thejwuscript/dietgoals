18ffb2668bffa626ad314c382f44dc539bd8ff86
James Wu <88938117+thejwuscript@users.noreply.github.com>
2023-12-28 22:13:48 -0800

    Change homepage food image

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <section className="flex flex-col items-center min-h-screen">
+    <div className="relative bg-[url('/images/berries_bg.jpg')] bg-center min-h-[900px]:bg-cover bg-no-repeat">
+      <section className="relative flex flex-col items-center min-h-screen">
         <div className="flex justify-center items-center text-2xl mt-12 gap-2">
           <Image src="/images/veggie_bowl.png" alt="veggie bowl" width={56} height={56} />
           <h1 className="mt-4 text-5xl font-bold">DietGoals</h1>
@@ -13,19 +13,14 @@ export default function Home() {
           <br />
           <span className="font-bold italic">reach your goals.</span>
         </div>
-        <div className="text-7xl my-10 px-4">Coming soon</div>
-        <div className="px-4 text-lg max-w-sm">
-          Join <span className="font-bold text-xl">28</span> individuals who are eager to start their path to a
-          healthier lifestyle!
+        <div className="text-7xl my-10 px-4 flex justify-center items-center">
+          <span className="w-min min-[500px]:w-full">Coming soon</span>
         </div>
-        <div className="flex justify-center flex-1 w-full bg-gradient-to-b from-transparent to-gray-300">
-          <Image
-            src="/images/plate.png"
-            alt="plate"
-            className="w-3/4 max-w-[400px] object-scale-down"
-            width={514}
-            height={512}
-          />
+        <div className="px-4 text-xl max-w-[400px] my-7 flex justify-center items-center">
+          <div>
+            Join <span className="font-bold text-2xl">28</span> individuals who are eager to start their path to a
+            healthier lifestyle!
+          </div>
         </div>
       </section>
     </div>
